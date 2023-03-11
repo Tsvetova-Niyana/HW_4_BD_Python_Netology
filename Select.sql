276b8c8ed448f905765a6a7164b20611e2174f94
Tsvetova-Niyana <92599629+Tsvetova-Niyana@users.noreply.github.com>
2023-03-11 20:05:30 +0300

    Change script "Select"

--- Select.sql
+++ Select.sql
@@ -3,69 +3,80 @@
 Внимание: результаты запросов не должны быть пустыми, при необходимости добавьте данные в таблицы.*/
 
 --Количество исполнителей в каждом жанре.
+
+/* нет необходимости группировать по айди. Здесь достаточно группировать по именам жанров.*/
+
 select 
 	g.name_genre, 	
 	count(*) as count_executor
 from executers_genres eg
 join genre g on eg.id_genre = g.id_genre
-group by eg.id_genre, g.name_genre
+group by g.name_genre
 order by g.name_genre;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов.
--- вариант 1 без использования подзапросов
-select 
-	a.name_album, 
-	a.year_release, 
-	count(*) as count_track 
-from track t
-join album a on t.id_album = a.id_album and a.year_release between 2019 and 2020
-group by t.id_album, a.name_album, a.year_release; 
+/*
+ * не реализуется в подзапросом и здесь не нужна группировка как и получение имени альбома с его годом. 
+ * В задании сказано получить общее количество треков в заданном диапазоне альбомов. 
+ */
 
--- вариант 2 с использованием подзапросов
 select 
-	a.name_album, 
-	a.year_release, 
 	count(*) as count_track 
 from track t
-join album a on t.id_album = a.id_album 
-where t.id_album in (select a2.id_album from album a2 where a2.year_release between 2019 and 2020)
-group by t.id_album, a.name_album, a.year_release;
+join album a on t.id_album = a.id_album and a.year_release between 2019 and 2020; 
 
 -- Средняя продолжительность треков по каждому альбому.
+/*
+ * реализован не совсем корректно. Необходимо получить имена альбомов и среднюю продолжительность по ним. 
+ * Выводя только идентификационные номера, нет понимание о каких альбомах идет речь.
+ */
+
 select 
-	t.id_album, 
-	count(*) as count_track, 
+	a.name_album, 
 	avg(t.duration_track) as avg_duration   
 from track t
-group by t.id_album
+join album a on a.id_album = t.id_album 
+group by t.id_album, a.name_album
 order by t.id_album; 
 
 -- Все исполнители, которые не выпустили альбомы в 2020 году.
+/* реализован не совсем корректно. Второй уровень вложенности запроса здесь излишен.*/
 
-select * from executor e
-where e.id_executor not in (select ae.id_executor from albums_executors ae 
-							where ae.id_album in (select a.id_album from album a 
-													where a.year_release = 2020 order by a.year_release));
+select e.nickname_executor from executor e
+where e.nickname_executor not in (select e2.nickname_executor  
+									from executor e2
+									join albums_executors ae on e2.id_executor = ae.id_executor
+									join album a on ae.id_album = a.id_album 
+									where a.year_release = 2020 )
+order by e.nickname_executor;
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+/*реализован не совсем корректно. 
+ * Во-первых, отсутствует объединение с таблице альбомов. 
+ * Во-вторых, вложенный запрос здесь не требуется. 
+ * Достаточно объединить таблицы и в условии where найти конкретного исполнителя. */
+
 select 
-	c.name_collection 
+	distinct c.name_collection 
 from collection c 
-where c.id_collection in (select 
-							ct.id_collection 
-						from collections_tracks ct 
-						join track t on ct.id_track = t.id_track
-						join albums_executors ae on t.id_album = ae.id_album 
-						join executor e on ae.id_executor = e.id_executor and e.nickname_executor = 'Коrsика');
+join collections_tracks ct on ct.id_collection = c.id_collection
+join track t on ct.id_track = t.id_track
+join album a on t.id_album = a.id_album
+join albums_executors ae on a.id_album = ae.id_album 
+join executor e on ae.id_executor = e.id_executor and e.nickname_executor = 'Коrsика';
 
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+
+/*реализован не совсем корректно. Здесь нет необходимости в реализации вложенного запроса. */
+
 select 
-	a.name_album 
+	distinct a.name_album 
 from album a 
-join albums_executors ae on ae.id_album = a.id_album 
-where ae.id_executor in (select eg.id_executor from executers_genres eg 
-						group by eg.id_executor
-						having Count(*) > 1)	
+join albums_executors ae on ae.id_album = a.id_album
+join executor e on ae.id_executor = e.id_executor
+join executers_genres eg on e.id_executor = eg.id_executor 
+group by a.name_album, e.nickname_executor  
+having Count(*) > 1	
 order by a.name_album;
 
 --Наименования треков, которые не входят в сборники.
@@ -76,22 +87,16 @@ order by t.name_track;
 
 -- Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, 
 -- — теоретически таких треков может быть несколько.
---вариант 1 через join
+
+/*пропустили объединение с таблице альбомов.*/
+
 select 
 	e.id_executor, 
 	e.nickname_executor
 from executor e
 join albums_executors ae on e.id_executor = ae.id_executor 
+join album a on ae.id_album = a.id_album 
 join track t on t.id_album = ae.id_album and t.duration_track in (select min(duration_track) from track t2); 
-
---вариант 2 через подзапросы
-select 
-	e.id_executor,
-	e.nickname_executor 
-from executor e 
-where e.id_executor in (select ae.id_executor from albums_executors ae
-						where ae.id_album in (select t.id_album from track t
-												where t.duration_track in (select min(duration_track) from track t2)));
 
 -- Названия альбомов, содержащих наименьшее количество треков.
 -- вариант 1
